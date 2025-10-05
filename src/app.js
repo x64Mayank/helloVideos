8af1fddab23c609e8eb560a0efa3a296518ccb65
Mayank Kumar <mayank.890.mk@gmail.com>
2025-10-05 14:05:00 +0530

    feat: add express app bootstrap and middleware base

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+//routes declaration
+app.use("/healthcheck", healthcheckRouter)
+app.use("/users", userRouter)
+app.use("/tweets", tweetRouter)
+app.use("/subscriptions", subscriptionRouter)
+app.use("/videos", videoRouter)
+app.use("/comments", commentRouter)
+app.use("/likes", likeRouter)
+app.use("/playlist", playlistRouter)
+app.use("/dashboard", dashboardRouter)
+
+export { app };
